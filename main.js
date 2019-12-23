ce86a7897fcb2ae88816ecaf1a5ae5e04a781d03
ComBarnea <com.barnea@gmail.com>
2019-12-23 19:38:58 +0200

    :sparkles: Add major changes to alot of stuff

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ let index = 0;
 //updating variable if there is and creating an empty one if not
 //and showing notes from local storage
 (function () {
-    if (localStorage.notesArrayString) {
-    notesArray = getNotesArrayFromLocalStorage();
-    }
-    else {
+    if (!localStorage.notesArrayString) {
         localStorage.notesArrayString = [];
+
     }
+
+    notesArray = getNotesArrayFromLocalStorage();
+    index = notesArray.length;
+
     displayAllNotes();
 }) ();
 
 function addNoteOnClick() {
-
     let userInputObject = getUserInput();
 
     clearRedBoxes(userInputObject);
@@ -38,51 +39,58 @@ function addNoteOnClick() {
 }
 
 function displayAllNotes() {
-    let container = document.getElementById("notesContainer");
-    container.innerHTML = "";
+    let notesContainer = document.getElementById('notesContainer');
+
+    notesContainer.innerHTML = '';
+
     for (let i = 0; i < notesArray.length; i++) {
-        addNoteTodisplay(notesArray[i]);
+        addNoteToDisplay(notesContainer, notesArray[i]);
     }
 }
 
-function addNoteTodisplay(note) {
-    let container = document.createElement("div");
-    let noteContent = document.createElement("div");
-    let noteDate = document.createElement("div");
-    let noteTime = document.createElement("div");
-    let xIcon = document.createElement("img");
+function addNoteToDisplay(notesContainer, note) {
+    let container = document.createElement('div');
+    let xIcon = document.createElement('button');
+    let noteContent = document.createElement('div');
+    let noteDate = document.createElement('div');
+    let noteTime = document.createElement('div');
 
-    noteContent.innerHTML = "<br>Task: " + note.noteContent;
-    noteDate.innerHTML = "Due date: " + note.noteDate;
-    noteTime.innerHTML = "Due time: " + note.noteTime;
+
+    noteContent.innerHTML = '<br>Task: ' + note.noteContent;
+    noteDate.innerHTML = 'Due date: ' + note.noteDate;
+    noteTime.innerHTML = 'Due time: ' + note.noteTime;
     container.id = note.id;
     xIcon.id = note.id;
 
-    noteContent.className = "noteContent";
-    noteDate.className = "noteDate";
-    noteTime.className = "noteTime";
-    xIcon.className = "deleteIcon";
+    noteContent.className = 'noteContent';
+    noteDate.className = 'noteDate';
+    noteTime.className = 'noteTime';
+    xIcon.className = 'close close-btn';
+    xIcon.type = 'button';
 
+    xIcon.innerHTML = '<span aria-hidden="true">&times;</span>';
+
+    container.appendChild(xIcon);
     container.appendChild(noteContent);
     container.appendChild(noteDate);
     container.appendChild(noteTime);
-    container.appendChild(xIcon);
-    notesContainer.appendChild(container); 
+
+    notesContainer.appendChild(container);
 
     xIcon.onclick = deleteNote;
 }
 
-
 function getUserInput() {
     let note = {
         id: index,
-        noteContent: document.getElementById("noteContent").value,
-        noteDate: document.getElementById("noteDate").value,
-        noteTime: document.getElementById("noteTime").value,
-        noteContentStyle: document.getElementById("noteContent").style,
-        noteDateStyle: document.getElementById("noteDate").style,
-        noteTimeStyle: document.getElementById("noteTime").style
-    }
+        noteContent: document.getElementById('noteContent').value,
+        noteDate: document.getElementById('noteDate').value,
+        noteTime: document.getElementById('noteTime').value,
+        noteContentStyle: document.getElementById('noteContent').style,
+        noteDateStyle: document.getElementById('noteDate').style,
+        noteTimeStyle: document.getElementById('noteTime').style
+    };
+
     index = index + 1;
     return note;
 }
@@ -92,57 +100,93 @@ function addToNotesArray(userInputObject) {
 }
 
 function getNotesArrayFromLocalStorage() {
-    return JSON.parse(localStorage.notesArrayString);
-    //return localStorage.getItem("notesArrayString");
+    return JSON.parse(localStorage.notesArrayString || '[]');
+    //return localStorage.getItem(''notesArrayString'');
 }
 
 function saveNoteToStorage(notesArray) {
     let notesArrayString = JSON.stringify(notesArray);
-    localStorage.setItem("notesArrayString", notesArrayString);
+    localStorage.setItem('notesArrayString', notesArrayString);
 }
 
 function deleteNote() {
     for (let i = 0; i < notesArray.length; i++) {
-        if (notesArray[i].id == this.id) {
+        if (String(notesArray[i].id) === this.id) {
             notesArray.splice(i, 1);
         }
     }
+
+    saveNoteToStorage(notesArray);
+
     displayAllNotes();
 }
 
 //clearing red boxes
 function clearRedBoxes(boxToClear){
-    boxToClear.noteContentStyle.backgroundColor = "";
-    boxToClear.noteDateStyle.backgroundColor = "";
-    boxToClear.noteTimeStyle.backgroundColor = "";
-//    document.getElementById("squareSpace").innerHTML = "";
+    boxToClear.noteContentStyle.backgroundColor = '';
+    boxToClear.noteDateStyle.backgroundColor = '';
+    boxToClear.noteTimeStyle.backgroundColor = '';
+//    document.getElementById(''squareSpace'').innerHTML = '''';
 }
 
 //clearing input from boxes
 function clearBoxesInput(inputReference) {
-    inputReference.noteContent.value = "";
+    inputReference.noteContent.value = '';
 }
 
+
+// == or === ?
 function validation(userInput) {
-    if (userInput.noteContent == "") {
-        errorMassege("Please write a task", userInput.noteContentStyle);
-        return false;
+    let errorMessageText = '';
+
+    if (userInput.noteContent === '') {
+        addErrorBackground(userInput.noteContentStyle);
+
+        let errorText = 'Please write a task.';
+
+        if (!errorMessageText) {
+            errorMessageText = errorText;
+        } else {
+            errorMessageText = errorMessageText + '\n' + errorText;
+        }
+
     }
 
-    if (userInput.noteDate == "") {
-        errorMassege("Please set a date", userInput.noteDateStyle);
-        return false;
+    if (userInput.noteDate === '') {
+        addErrorBackground(userInput.noteDateStyle);
+
+        let errorText = 'Please set a date.';
+
+        if (!errorMessageText) {
+         errorMessageText = errorText;
+        } else {
+            errorMessageText = errorMessageText + '\n' + errorText;
+        }
     }
 
-    if (userInput.noteTime == "") {
-        errorMassege("Please set task due time", userInput.noteTimeStyle);
-        return false;
+    if (userInput.noteTime === '') {
+        addErrorBackground(userInput.noteTimeStyle);
+
+        let errorText = 'Please set task due time.';
+
+        if (!errorMessageText) {
+         errorMessageText = errorText;
+        } else {
+            errorMessageText = errorMessageText + '\n' + errorText;
+        }
     }
 
-    return true;
+    if (!errorMessageText) return true;
+
+    errorMessage(errorMessageText);
+
+    return false;
 }
 
-function errorMassege(textMassege, boxToMark) {
-    alert(textMassege);
-    boxToMark.backgroundColor = "red";
+function addErrorBackground(boxToMark) {
+    boxToMark.backgroundColor = 'red';
+}
+
+function errorMessage(textMessage) {
+    alert(textMessage);
 }
